11ff9372ad402110ce5c064ce4a639c45ae5f960
IClickTech <IClick.Tech.2014@gmial.com>
2025-01-13 22:47:37 +0200

    End of Section 3

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    //[Route("Regions")]
+    //[Route("api/[controller]")]
+    [Route("[controller]")]
+
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegions()
+        {
+            var regionsDomain = await regionRepository.GetAllAsync();
+
+            var regionsDto = mapper.Map<List<RegionDto>>(regionsDomain);
+
+            //var regionsDto = new List<RegionDto>();            
+            //foreach (var region in regionsDomain)
+            //{
+            //    regionsDto.Add(new RegionDto()
+            //    {
+            //        Id = region.Id,
+            //        Code = region.Code,
+            //        Name = region.Name,                    
+            //        Area = region.Area,
+            //        Lat = region.Lat,
+            //        Long = region.Long,
+            //        Population = region.Population
+            //    });
+            //}
+
+            return Ok(regionsDto);
+        }
+        
+    }
+}
